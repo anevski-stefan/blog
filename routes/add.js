e98e46ac9213f8b9a7639cf8524b44fc5dbde1d1
Stefan Anevski <stefan.anevski@one-inside.com>
2023-08-12 23:11:47 +0200

    Implement ckEditor and install new plugins

--- routes/add.js
+++ routes/add.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const client = require("../database/database.js");
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const window = new JSDOM("").window;
+const DOMPurify = createDOMPurify(window);
 
 router.post("/blogs/add", (req, res) => {
   const title = req.body.title;
-  const content = req.body.content;
+  const content = DOMPurify.sanitize(req.body.content);
   const date = new Date();
   const query =
     "INSERT INTO blog(title, content, createdat) VALUES($1, $2, $3)";
