3c1b9eb7ef83dc7d29fc4fd4d0ac58be9f957c2d
Stefan Anevski <stefan.anevski@one-inside.com>
2023-08-24 14:46:42 +0200

    Implement update user info functionality

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,13 +5,29 @@ const client = require("../database/database.js");
 router.get("/:username/dashboard", (req, res) => {
   const username = req.params.username;
   const query = "SELECT * FROM bloguser WHERE username = $1";
-
   client.query(query, [username], (err, result) => {
     if (err) {
       console.log(err.message);
       return;
     }
     res.render("dashboard", { user: result.rows[0] });
+  });
+});
+
+router.post("/:username/update", (req, res) => {
+  const firstname = req.body.fname;
+  const lastname = req.body.lname;
+  const username = req.params.username;
+
+  const query =
+    "UPDATE bloguser SET firstname = $1, lastname = $2 WHERE username = $3";
+  client.query(query, [firstname, lastname, username], (err, result) => {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    console.log("User information updated successfully.");
+    res.redirect(`/${username}/dashboard`);
   });
 });
 
--- routes/home.js
+++ routes/home.js
@@ -3,36 +3,7 @@ const router = express.Router();
 const client = require("../database/database.js");
 
 router.get("", (req, res) => {
-  const query = `SELECT *, 
-  COALESCE(like_counts.like_count, 0) AS like_count, 
-  COALESCE(dislike_counts.dislike_count, 0) AS dislike_count 
-  FROM blog LEFT JOIN (
-    SELECT 
-      id_blog, 
-      COUNT(*) AS like_count 
-    FROM 
-      blog_like 
-    GROUP BY 
-      id_blog
-  ) AS like_counts ON blog.id = like_counts.id_blog
-  LEFT JOIN (
-    SELECT 
-      id_blog, 
-      COUNT(*) AS dislike_count 
-    FROM 
-      blog_dislike 
-    GROUP BY 
-      id_blog
-  ) AS dislike_counts ON blog.id = dislike_counts.id_blog
-  ORDER BY createdat DESC LIMIT 5;`;
-
-  client.query(query, (err, result) => {
-    if (err) {
-      console.log(err.message);
-      return;
-    }
-    res.render("index", { blogs: result.rows });
-  });
+  res.render("index");
 });
 
 module.exports = router;
